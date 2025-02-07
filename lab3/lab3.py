c383e5feab2f0cbcb08ad17711d871dd08b07fad
aacting <159343030+aacting@users.noreply.github.com>
2025-02-07 11:04:29 +0300

    Update lab3.py

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,6 +1,5 @@
 class Book:
     """ Базовый класс книги.
-
     Атрибуты:
         _name (str): Название книги.
         _author (str): Автор книги.
@@ -8,7 +7,6 @@ class Book:
 
     def __init__(self, name: str, author: str):
         """Инициализирует экземпляр класса Book.
-
         Аргументы:
             name (str): Название книги.
             author (str): Автор книги.
@@ -37,14 +35,12 @@ class Book:
 
 class PaperBook(Book):
     """ Дочерний класс. Бумажная книга.
-
     Атрибуты:
         pages (int): Количество страниц в книге.
     """
 
     def __init__(self, name: str, author: str, pages: int):
         """Инициализирует экземпляр класса PaperBook.
-
         Аргументы:
             name (str): Название книги.
             author (str): Автор книги.
@@ -61,10 +57,8 @@ class PaperBook(Book):
     @pages.setter
     def pages(self, value):
         """Устанавливает количество страниц в книге.
-
         Аргументы:
             value (int): Количество страниц.
-
         Исключения:
             TypeError: Если количество страниц не является целым числом.
             ValueError: Если количество страниц меньше или равно 0.
@@ -75,10 +69,6 @@ class PaperBook(Book):
             raise ValueError("В книге должна быть хотя бы одна страница")
         self._pages = value
 
-    def __str__(self):
-        """Возвращает строковое представление бумажной книги."""
-        return f"Книга {self.name}. Автор {self.author}. Количество страниц {self.pages}"
-
     def __repr__(self):
         """Возвращает формальное строковое представление бумажной книги."""
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
@@ -86,14 +76,12 @@ class PaperBook(Book):
 
 class AudioBook(Book):
     """ Дочерний класс. Аудиокнига.
-
     Атрибуты:
         duration (float): Продолжительность аудиокниги в часах.
     """
 
     def __init__(self, name: str, author: str, duration: float):
         """Инициализирует экземпляр класса AudioBook.
-
         Аргументы:
             name (str): Название книги.
             author (str): Автор книги.
@@ -110,10 +98,8 @@ class AudioBook(Book):
     @duration.setter
     def duration(self, value):
         """Устанавливает продолжительность аудиокниги.
-
         Аргументы:
             value (float): Продолжительность аудиокниги.
-
         Исключения:
             TypeError: Если продолжительность не является числом.
             ValueError: Если продолжительность меньше или равна 0.
@@ -123,10 +109,6 @@ class AudioBook(Book):
         if value <= 0:
             raise ValueError("Продолжительность книги должна быть больше 0")
         self._duration = value
-
-    def __str__(self):
-        """Возвращает строковое представление аудиокниги."""
-        return f"Книга {self.name}. Автор {self.author}. Продолжительность {self.duration}"
 
     def __repr__(self):
         """Возвращает формальное строковое представление аудиокниги."""
